3e0186b64b674575b5e09ae1d588421de61c7ac5
Ruben <ruben.mte@gmail.com>
2023-04-12 15:23:16 +0200

    Creacion de archivo env

--- pom/tests/login.spec.js
+++ pom/tests/login.spec.js
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
+import dotenv from 'dotenv'
+dotenv.config()
 
 test('User logs correctly', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
+    await page.goto(process.env.SAUCEDEMOURL);
     const loginPage = new LoginPage(page);
-    await loginPage.submitLoginForm('standard_user','secret_sauce')
+    await loginPage.submitLoginForm(process.env.SAUCEDEMOUSER,process.env.SAUCEDEMOPASS)
 
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 });
